d838a34d3e51b22e4b5b47098653139f074d3709
Nicolas <zhaoxd@zenmen.com>
2019-12-16 14:09:34 +0800

    fix: 搞错了 banner 名

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ export default {
       name: toCamelCaseName(pkgName),
       file: `dist/${pkgName}.js`,
       format: 'umd',
-      banner: `/* ${pkg.name} v${pkg.version} by ${pkg.author} */`,
+      banner: `/* ${toCamelCaseName(pkgName)} v${pkg.version} by ${pkg.author} */`,
     },
     {
       file: pkg.main,
